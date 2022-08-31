2e37f0d259cb793df07646f62f93d968a75209f7
Loïc Billon <loic.billon@gmail.com>
2022-08-31 16:39:02 +0200

    GetVocabulaires

---
+++ opentoutatice-ecm-platform-core/src/main/java/fr/toutatice/ecm/platform/core/constants/ToutaticeUtilsConst.java
@@ -0,0 +1,28 @@
+/*
+ * (C) Copyright 2014 Académie de Rennes (http://www.ac-rennes.fr/), OSIVIA (http://www.osivia.com) and others.
+ *
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the GNU Lesser General Public License
+ * (LGPL) version 2.1 which accompanies this distribution, and is available at
+ * http://www.gnu.org/licenses/lgpl-2.1.html
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ *
+ * Contributors:
+ *   mberhaut1
+ *    
+ */
+package fr.toutatice.ecm.platform.core.constants;
+
+public class ToutaticeUtilsConst {
+    
+    private ToutaticeUtilsConst(){};
+    
+	public final static String CST_DEFAULT_DIRECTORIES_SEPARATE_CHARACTER = "/";
+	public final static String CST_DEFAULT_BUNDLE_NAME = "messages";
+	public final static String PATH_SEPARATOR = "/";
+}
